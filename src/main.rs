58a103fb36833b614fcf792fbc138e2d2367bbed
Omansh Choudhary <omansh.choudhary@gmail.com>
2026-05-15 00:04:05 +0530

    feat(cli): setup basic command line routing with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser, Subcommand};
+use anyhow::Result;
+
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about=None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    /// Initialize a new and empty repo
+    Init,
+}
+
+fn main()->Result<()> {
+    let cli = Cli::parse();
+    
+    match cli.command {
+        Command::Init => {
+            println!("Initializing repository");
+        } 
+    }
+    
+    Ok(())    
 }
